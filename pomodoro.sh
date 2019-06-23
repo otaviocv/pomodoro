ce796fe0f014e9cc8c9b4b2a76361f05e0f8c77e
Otávio Vasques <otaviocv.deluqui@gmail.com>
2019-06-23 13:13:55 -0300

    Add more stable functionalities to pomodoro and tomato png

--- pomodoro.sh
+++ pomodoro.sh
@@ -1,27 +1,8 @@
 #!/bin/bash -e
 
-SHORT_CYCLE_LENGHT=15 # in minutes
+SHORT_CYCLE_LENGHT=5 # in minutes
 NUMBER_OF_SHORT_BREAKS=4 # The nuber of short breaks until a long break
-
-notify-send "Starting cycles. You will see a notification like this."
-while true
-do
-    for i in {1..$((${NUMBER_OF_SHORT_BREAKS}))}
-    do
-        sleep ${SHORT_CYCLE_LENGHT}
-        message="$(short_cycle_message)"
-        notify-send "${message}"
-        say "${message}"
-        echo "${i}"
-        echo "${message}"
-    done
-    sleep ${SHORT_CYCLE_LENGHT}
-    long_message="$(long_cycle_message)"
-    notify-send "${long_message}"
-    say "${long_message}"
-
-done
-
+LONG_BREAK_DURATION=5 # in minutes
 
 short_cycle_message () {
     possible_messages=(
@@ -29,17 +10,60 @@ short_cycle_message () {
     "Stand up!"
     "Watch your posture!"
     )
-    number_of_possible_messages=5
+    number_of_possible_messages=3
     echo ${possible_messages[$((RANDOM % number_of_possible_messages))]}
 }
-
 
 long_cycle_message () {
     possible_messages=(
     "LOOOONG BREAK!"
-    "Stand up, take a coffee, look at the window then come back to work."
+    "Long break. Stand up, take a coffee, look at the window then come back to work."
     "Long break. Please stop working."
     )
     number_of_possible_messages=3
     echo ${possible_messages[$((RANDOM % number_of_possible_messages))]}
 }
+
+end_of_rest_message () {
+    possible_messages=(
+    "Ending of rest period, get back to work."
+    "Your rest period just finished, get back here."
+    )
+    number_of_possible_messages=2
+    echo ${possible_messages[$((RANDOM % number_of_possible_messages))]}
+}
+
+message_notification () {
+    notify-send "${1}"
+    say "${1}"
+    echo "$(date) [${2}]: \"${1}\""
+}
+
+short_cycle () {
+    sleep ${SHORT_CYCLE_LENGHT}
+    log_message="$(message_notification "$(short_cycle_message)" "SHORT CYCLE")"
+    echo ${log_message}
+}
+
+last_short_cycle () {
+    sleep ${SHORT_CYCLE_LENGHT}
+    log_message="$(message_notification "$(long_cycle_message)" "LONG CYCLE")"
+    echo ${log_message}
+}
+
+rest_cycle () {
+    sleep ${LONG_BREAK_DURATION}
+    log_message="$(message_notification "$(end_of_rest_message)" "REST")"
+    echo ${log_message}
+}
+
+notify-send "Starting cycles. You will see a notification like this."
+while true
+do
+    for i in $( seq 1 $((${NUMBER_OF_SHORT_BREAKS}-1)) )
+    do
+        echo "$(short_cycle)"
+    done
+    echo "$(last_short_cycle)"
+    echo "$(rest_cycle)"
+done
